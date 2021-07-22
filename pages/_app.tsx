315a639f7d914b834be14f6ceb56735f5a5ef01c
Luke Shay <38476395+LukeShay@users.noreply.github.com>
2021-07-22 18:27:10 +0000

    removed unused imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,12 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import '../styles/globals.css';
 import 'react-toastify/dist/ReactToastify.css';
-import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
-import Navbar from '@/components/Navbar';
 import React from 'react';
-import config from '@/client/config';
-import flowers from '../public/flowers.png';
 import { ToastContainer, Slide } from 'react-toastify';
 import { configureAPI } from '@/client/api';
-import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: { Component: any; pageProps: any }) {
   configureAPI();
-
-  const router = useRouter();
 
   return (
     <>
