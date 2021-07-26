16c2ee9072ee6257213cec4eab96a0f3a36a25ba
Luke Shay <shay.luke17@gmail.com>
2021-07-25 19:49:05 -0500

    change to img

--- components/Container.tsx
+++ components/Container.tsx
@@ -9,7 +9,7 @@ export default function Container({ children }: ContainerProps) {
   return (
     <BaseContainer>
       <div className="lg:sticky top-0 z-0 w-full -mb-10 lg:-mb-72 xl:-mb-96">
-        <img src="/images/flowers.ong" alt="flowers" />
+        <img src="/images/flowers.png" alt="flowers" />
       </div>
       <main className="min-h-screen">{children}</main>
     </BaseContainer>
