7d097fa5effd7cd72902200329ade3e5ae08df39
Luke Shay <38476395+LukeShay@users.noreply.github.com>
2021-07-29 21:26:14 +0000

    removed digital ocean from pulumi

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,9 +1,1 @@
-import * as cloudflare from './src/cloudflare';
-
-let digitalocean = null;
-
-if (process.env.SKIP_DO !== 'true') {
-  digitalocean = require('./src/digitalocean');
-}
-
-export { digitalocean, cloudflare };
+export * from './src/cloudflare';
